66ade90a46f5a9f8a64dd5e7cab9e6a5d77f9882
Harsh Garg <gargharsh2512@gmail.com>
2022-08-30 12:15:17 +0530

    Menu works
    Have to add the options inside the menu

--- script.js
+++ script.js
@@ -4,24 +4,22 @@ const container = document.querySelector(".container")
 
 button.addEventListener("click", () => {
     makeVisible()
-})
+}, true)
 
 function makeVisible() {
     menu.classList.add("visible")
     
-    setTimeout(3)
-    container.addEventListener("click", () => {
-        button.removeEventListener("click", vanish)
-        vanish()
-    })
 }
 
+container.addEventListener("click", () => {
+    button.removeEventListener("click", vanish)
+    vanish()
+}, true)
 
 
 function vanish() {
-    console.log(menu.classList.contains("visible"));
+
     if(menu.classList.contains("visible")) {
         menu.classList.remove("visible")
-        console.log("HI");
     }
 }
